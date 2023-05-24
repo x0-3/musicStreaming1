6e653b2b0a43fd7be22936583c003c3df2d57994
x0-3 <steph6363@gmail.com>
2023-05-24 21:38:55 +0200

    role edit set in userEdit

--- src/Form/EditUserType.php
+++ src/Form/EditUserType.php
@@ -10,6 +10,7 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class EditUserType extends AbstractType
