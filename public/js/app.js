7447eab6e7e05bbefaebb9f46aea8f76c66065b3
x0-3 <steph6362@gmail.com>
2023-05-24 14:24:58 +0200

    modal added / fixed css

--- public/js/app.js
+++ public/js/app.js
@@ -67,6 +67,34 @@ window.closeNav = function() {
 }
 
 
+// ********************************************** Modal ****************************************************************** * // 
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on close btn, close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
 // ********************************************* add comment form *****************************************************************//
 const formComment = document.querySelector('#commentForm'); 
 const commentList = document.querySelector('.comments');
