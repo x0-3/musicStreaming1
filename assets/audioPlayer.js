6759963b6e6bbb038c67b91402de2e998c4f0c21
x0-3 <steph6362@gmail.com>
2023-08-05 22:00:57 +0200

    function commented

--- assets/audioPlayer.js
+++ assets/audioPlayer.js
@@ -114,11 +114,13 @@ function switchtrack(){
 
 // ************************************************ music time ************************************************//
 
+// if there the id of the audio "#audio" is present in the page
 if(audio){
 
+  // when the audio metadata is loaded
   audio.onloadedmetadata = function() {
     
-    
+    // function to format and display time in minutes and seconds
     function setTime(output, input){
     
       // calculate the minutes
@@ -127,36 +129,38 @@ if(audio){
       // calculate the seconds
       const seconds = Math.floor(input % 60);
     
-      // if seconds is under 10
+      // if seconds is under 10, display with a leading 0
       if (seconds < 10) {
         
-        // show a zero 
+        // add a zero to the seconds "2:05"
         output.innerHTML = minutes + ":0" + seconds;
     
       }else{
     
-        // show the :
+        // show the :"3:21"
         output.innerHTML = minutes + ":" + seconds;
       }
     }
     
+    // set the total duration of the audio as fulltime by using the setTime function
     setTime(fulltime, audio.duration);
   
-    // current time on slider 
+    // listen for updates in the audio playback time
     audio.addEventListener('timeupdate', () => {
       
-      // get the current audio time
+      // get the current audio time in seconds
       const currentAudioTime = Math.floor(audio.currentTime);
   
   
-      // get the percentage
+      // calculate the percentage of audio played
       const timePercentage = (currentAudioTime / audio.duration) * 100 + "%";
   
-      // set the current time of the song
+      // update the displayed time with the setTime function
       setTime(time, currentAudioTime);
   
-      // set the slider to the time
+      //set the width of the progress bar to represent music progress
       progress.style.width = timePercentage;
+      // set the position of the slider to represent the music progress
       thumb.style.left = timePercentage;
   
     });
